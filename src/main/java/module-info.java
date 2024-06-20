80437e8f04ab68207e711abeabe1bd10d6288b39
luiz-henrique112 <108096881+luiz-henrique112@users.noreply.github.com>
2024-06-20 09:58:13 -0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module lista {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires org.controlsfx.controls;
-
-    opens lista to javafx.fxml;
-    exports lista;
-}
